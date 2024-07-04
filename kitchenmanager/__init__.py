136331760a979fcbba59044fadb3f3a8ab0845f2
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-04 15:19:35 +0100

    Feat: Create base Flask-SQLAlchemy application
    Created the kitchenmanager package with a base route. Updated README and created requirements.txt

---
+++ kitchenmanager/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from sqlalchemy.orm import DeclarativeBase # type: ignore
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+db.init_app(app)
---
+++ kitchenmanager/routes.py
@@ -0,0 +1,6 @@
+from flask import render_template
+from kitchenmanager import app, db
+
+@app.route('/')
+def home():
+    return render_template('base.html')
