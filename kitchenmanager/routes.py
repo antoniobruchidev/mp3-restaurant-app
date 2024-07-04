d24e248645225aeecf8b12fb5f510cd86bb61702
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-04 21:25:09 +0100

    Feat: Add login modal
    Added a modal in base template from which users and employees will be able to login choosing either google or metamask. Also added base js to handle google response

--- kitchenmanager/routes.py
+++ kitchenmanager/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import jsonify, render_template
 from kitchenmanager import app, db
 from kitchenmanager.models import Message, User, Wallet, Supplier, BoughtItem, ManufactoredItem, Recipe, SellableItem, StockMovement, Order, Delivery
@@ -5,7 +6,7 @@ from kitchenmanager.models import Message, User, Wallet, Supplier, BoughtItem, M
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', g_client_id=os.environ.get("GOOGLE_CLIENT_ID"))
 
 
 @app.route('/menu')
--- kitchenmanager/static/js/script.js
+++ kitchenmanager/static/js/script.js
@@ -1,3 +1,23 @@
+/**
+ * Function to decode Google user credentials
+ * @param {*} credential 
+ * @returns 
+ */
+const decodeJWT = (credential) => {
+  var tokens = credential.split('.');
+  return JSON.parse(atob(tokens[1]));
+}
+
+/**
+ * Function to handle Google Signin response
+ * @param {*} response 
+ */
+function handleCredentialResponse(response) {
+  const userCredential = decodeJWT(response.credential);
+  console.log(userCredential.sub, userCredential.name, userCredential.email);
+}
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.modal').modal();
   });
