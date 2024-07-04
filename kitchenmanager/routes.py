ba1b18f3861bc5f7205aed1bcb2f81b69dfbee08
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-04 18:04:19 +0100

    Feat: Add dattabase models
    Added all the models and many to many relationship tables. Added import in routes. Updated requirements.txt

--- kitchenmanager/routes.py
+++ kitchenmanager/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from kitchenmanager import app, db
+from kitchenmanager.models import Message, User, Wallet, Supplier, BoughtItem, ManufactoredItem, Recipe, SellableItem, StockMovement, Order, Delivery
+
 
 @app.route('/')
 def home():
