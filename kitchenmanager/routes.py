e7fb0a7adb026dd8e74bee4dfa458b8bc00b2272
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-04 20:36:04 +0100

    Feat: Set up frontend framework
    Added Materialize framework, jQuery and FontAwesome. Also added a navigation bar, relative endpoints in routes and pages in templates

--- kitchenmanager/routes.py
+++ kitchenmanager/routes.py
@@ -1,8 +1,18 @@
-from flask import render_template
+from flask import jsonify, render_template
 from kitchenmanager import app, db
 from kitchenmanager.models import Message, User, Wallet, Supplier, BoughtItem, ManufactoredItem, Recipe, SellableItem, StockMovement, Order, Delivery
 
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+
+@app.route('/seat')
+def seat():
+    return render_template('seat.html')
