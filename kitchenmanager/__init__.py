1f702fa8c14affee06b2a0ed7c2f32b5ab87d7df
Antonio Bruchi <antoniobruchi@hotmail.com>
2024-07-04 15:25:44 +0100

    Maint: Fix routes import
    Added missing import in __init__.py

--- kitchenmanager/__init__.py
+++ kitchenmanager/__init__.py
@@ -16,3 +16,5 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db.init_app(app)
+
+from kitchenmanager import routes #noqa
